1a1651eaf6f904ba52668e0ae4b3abd9bd64ad2a
Matheus Bortolotto <matheusbortolol@hotmail.com>
2024-04-26 22:46:40 -0300

    Aula sobre while e for

---
+++ script.js
@@ -0,0 +1,60 @@
+// const i = 0
+// while(i <=10){
+//     console.log(i)
+//     i++
+// }
+
+
+
+
+//Meu codigo 
+// const calcular = (numero) =>{
+//     const pegarNumero = document.getElementById("numero").value
+//     let mudarNumero = Number(pegarNumero) 
+//     if( mudarNumero >0){
+//         while(mudarNumero !=0){
+//             console.log(mudarNumero)
+//             mudarNumero--
+//         } 
+   
+//     }
+//     else {
+//         alert("valores invalidos")
+//     }
+// }
+
+// for (i=0; i<10; i+=2){
+//     console.log(i)
+// }
+
+
+// for (i=10; i <usuarios.lemgth ; i-=2){
+//     console.log(i)
+// }
+
+
+// function calcular(){
+//     const pegarNumero = document.getElementById("numero").value
+//     let mudarNumero = Number(pegarNumero) 
+//     for ( i=0; i<=10; i++){
+//         console.log(` A soma do numero é ${mudarNumero} X ${i}= ${mudarNumero * i} `)
+//     }
+// }
+
+
+// function calcular(){
+//     const pegarNumero = document.getElementById("numero").value
+//     let mudarNumero = Number(pegarNumero) 
+//     let resp = 1
+//     if(mudarNumero > 0){
+//         while(resp <= mudarNumero){
+//             if(mudarNumero % resp == 0)
+//             console.log(resp)
+//         }
+//         resp ++
+   
+//     }   else{
+//     alert("valores invalidos")
+
+// }
+
